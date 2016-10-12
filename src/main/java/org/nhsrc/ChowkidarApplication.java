ba6d0baa17c76c7bf28f219e9d2d32519b6e72db
mihirk <mihir.khatwani@gmail.com>
2016-10-12 09:20:49 +0530

    Init

---
+++ src/main/java/org/nhsrc/ChowkidarApplication.java
@@ -0,0 +1,12 @@
+package org.nhsrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChowkidarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChowkidarApplication.class, args);
+	}
+}
